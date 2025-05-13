4a5b3bc508c59b9779ec6ba7a956def071677bcf
Abate <abateagegnehu574@gmail.com>
2025-05-13 18:33:13 +0300

    this is from branch abate

---
+++ abate.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const abate = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, qui!
+      Autem magni debitis quae excepturi exercitationem ea laudantium neque
+      illum ex rerum accusamus alias, consequuntur libero cumque, maxime
+      deleniti officia?
+    </div>
+  );
+};
+
+export default abate;
